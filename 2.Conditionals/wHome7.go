c94aece5ff014d63d08701e0a899abdda44b682c
DoniyorAli <doniyorfarhodov96@gmail.com>
2022-07-12 17:19:51 +0500

    I added Calculating Leap years

---
+++ 2.Conditionals/wHome7.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	year := 0
+	fmt.Printf("Enter year:")
+	fmt.Scanf("%d", &year)
+
+	if year % 400 == 0 || (year % 4 == 0 && year % 100 != 0) {
+		fmt.Println("Leap year")
+	}else {
+		fmt.Println("Not leap year!")
+	}
+}
