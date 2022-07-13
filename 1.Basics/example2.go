bf248d38aa9d008624a3a145c2742cf87f4268c2
DoniyorAli <doniyorfarhodov96@gmail.com>
2022-07-13 23:15:51 +0500

    Added varibles int, float, string, bool and deleted the extras

--- 1.Basics/example2.go
+++ 1.Basics/example2.go
@@ -1,19 +1,6 @@
 package main
-/*
-var name string = "Alixan"   // GLOBAL
 
-var (
-	box1 string = "Hello"
-	box2 int = 11
-	box3 float32 = 22.3
-	box4 bool = true
-)
-*/
 func main(){ // STRING, NUMBER, BOOLEAN
-/*
-	println(name)
-	println(box1, box2, box3, box4)
-*/
 
 // STRING
 
