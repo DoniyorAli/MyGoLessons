845dd060ce318eb5ccb29e80a98d020885277dfa
DoniyorAli <doniyorfarhodov96@gmail.com>
2022-07-15 19:28:58 +0500

    I added time.Sleep library method with examples

---
+++ 3.Loops/example3.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"time"
+	)
+
+func main() {
+
+	var number int = 0
+	for number <= 11 {
+		fmt.Println("-->", number)
+		number++
+		time.Sleep(1 * time.Second)
+	}
+	fmt.Println("Next time ⏰")
+//__________________________________________________
+	fmt.Print("\n")
+
+	var number2 int = 12
+	for number2 <= 20 {
+		fmt.Println("-->", number2)
+		number2++
+		time.Sleep(500 * time.Millisecond)
+	}
+	fmt.Println("The End ⏰")
+
+}
