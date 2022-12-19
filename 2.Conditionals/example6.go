cc6ca4b366ea9cd45da557032008aba80b482baa
DoniyorAli <doniyorfarhodov96@gmail.com>
2022-12-19 20:33:19 +0500

    even and odd number with conditional

--- 2.Conditionals/example6.go
+++ 2.Conditionals/example6.go
@@ -8,9 +8,9 @@ func main() {
 	fmt.Println("Even or odd programm, enter the number: ")
 	fmt.Scanf("%d", &number)
 
-	if number % 2 == 0 {
+	if number%2 == 0 {
 		fmt.Println(number, "Even number")
-	}else{
+	} else {
 		fmt.Println(number, "An odd number")
 	}
 
