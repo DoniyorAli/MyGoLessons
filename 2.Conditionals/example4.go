6990209346b0e1fee306223d308dba1395a07ee7
DoniyorAli <doniyorfarhodov96@gmail.com>
2022-12-19 20:30:09 +0500

    logical operators

--- 2.Conditionals/example4.go
+++ 2.Conditionals/example4.go
@@ -1,18 +1,18 @@
-package main    // LOGICAL OPERATORS    || and && and !
-				  
+package main // LOGICAL OPERATORS    || and && and !
+
 import "fmt"
 
 func main() {
 
 	// || --> OR (yoki) --> +
-	fmt.Println(true || true)   //  1 + 1 = 1 --> true 
+	fmt.Println(true || true)   //  1 + 1 = 1 --> true
 	fmt.Println(true || false)  //  1 + 0 = 1 --> true
 	fmt.Println(false || true)  //  0 + 1 = 1 --> true
 	fmt.Println(false || false) //  0 + 0 = 0 --> false
 
 	// && --> AND (va) --> *
 	fmt.Print("\n")
-	fmt.Println(true && true)   //  1 * 1 = 1 --> true 
+	fmt.Println(true && true)   //  1 * 1 = 1 --> true
 	fmt.Println(true && false)  //  1 * 0 = 0 --> false
 	fmt.Println(false && true)  //  0 * 1 = 0 --> false
 	fmt.Println(false && false) //  0 * 0 = 0 --> false
@@ -26,9 +26,9 @@ func main() {
 	fmt.Println(true && false || true)   // --> true
 	fmt.Println(false || true && false)  // --> false
 
-	// ! --> 
+	// ! -->
 	fmt.Print("\n")
-	fmt.Println(!true)   // --> false
-	fmt.Println(!false)  // --> true
+	fmt.Println(!true)  // --> false
+	fmt.Println(!false) // --> true
 
 }
